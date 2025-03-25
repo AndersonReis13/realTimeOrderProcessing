be38cd60e51a7c26ad586a644735c69b1868f148
anderson.reis <andersonline62@gmail.com>
2025-03-25 11:16:43 -0300

    fix: adicioando a logica do updateOrder;

--- src/main/java/com/as/_3/realtimeorderprocessing/application/usecases/orders/impl/UpdateOrderStatusUseCaseImpl.java
+++ src/main/java/com/as/_3/realtimeorderprocessing/application/usecases/orders/impl/UpdateOrderStatusUseCaseImpl.java
@@ -1,11 +1,12 @@
 package com.as._3.realtimeorderprocessing.application.usecases.orders.impl;
 
-import com.as._3.realtimeorderprocessing.application.exceptions.orders.IllegalArgumentOrder;
+import com.as._3.realtimeorderprocessing.application.exceptions.orders.OrderNotFoundException;
 import com.as._3.realtimeorderprocessing.application.usecases.orders.UpdateOrderStatusUseCase;
 import com.as._3.realtimeorderprocessing.core.entites.Order;
 import com.as._3.realtimeorderprocessing.core.enums.OrderStatus;
 import com.as._3.realtimeorderprocessing.core.gateways.OrderGateways;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public class UpdateOrderStatusUseCaseImpl implements UpdateOrderStatusUseCase {
@@ -19,8 +20,21 @@ public class UpdateOrderStatusUseCaseImpl implements UpdateOrderStatusUseCase {
 
     @Override
     public Order updateOrderStatus(Long orderId, String status) {
-       Optional<Order> order = orderGateways.getOrderById(orderId);
+       Order order = orderGateways.getOrderById(orderId);
 
-       return null;
+       if(order == null){
+           throw new OrderNotFoundException("Nenhum order foi encontrado!");
+       }
+
+       Order newOrder = new Order(
+               orderId,
+               order.customerName(),
+               order.totalAmount(),
+               OrderStatus.PAGO,
+               order.createdAt(),
+               LocalDate.now()
+       );
+
+       return newOrder;
     }
 }
